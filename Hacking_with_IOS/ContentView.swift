188bec9b49ca8d8565bae0d79fd59b2409cce4c9
Luiz Gall <luiz_gall@hotmail.com>
2026-01-02 11:45:08 -0300

    Adding WeSplit app template

--- Hacking_with_IOS/ContentView.swift
+++ Hacking_with_IOS/ContentView.swift
@@ -8,14 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount: Double = 0.0
+    @State private var numberOfPeople: Int = 2
+    @State private var tipPercentage: Int = 20
+    private var totalPerPerson: Double {
+        return (checkAmount + (checkAmount * Double(tipPercentage) / 100.0)) / Double(numberOfPeople)
+    }
+    
+    private let tipOptions: [Int] = [10, 15, 20, 25, 30]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationStack {
+            Form {
+                Section{
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                    
+                }
+                
+                Section {
+                        Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                Section {
+                        Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }.pickerStyle(.navigationLink)
+                }
+                
+                Section ("How much would you like to tip?"){
+                    Picker("Tip percentage?", selection: $tipPercentage) {
+                        ForEach(tipOptions, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }.pickerStyle(.segmented)
+                }
+                
+                Section {
+                    Text("Total per person: \(totalPerPerson)")
+                }
+            }
+        }.navigationTitle("WeSplit")
+        
     }
 }
 
