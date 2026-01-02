7705e775a3e4785919ea4163af0cfe7fc503ac14
Luiz Gall <luiz_gall@hotmail.com>
2026-01-01 21:43:10 -0300

    Initial Commit

---
+++ Hacking_with_IOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hacking_with_IOS
+//
+//  Created by Luiz Gall on 2026-01-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hacking_with_IOS/Hacking_with_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacking_with_IOSApp.swift
+//  Hacking_with_IOS
+//
+//  Created by Luiz Gall on 2026-01-01.
+//
+
+import SwiftUI
+
+@main
+struct Hacking_with_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
